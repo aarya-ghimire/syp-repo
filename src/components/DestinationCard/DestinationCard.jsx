1acdd8840bf41081ea3eb68d2e76bb7d785db2ec
aarya-ghimire <aaryaghimire36@gmail.com>
2024-11-26 20:46:41 +0545

    new commit

--- src/components/DestinationCard/DestinationCard.jsx
+++ src/components/DestinationCard/DestinationCard.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from '../../styles/DestinationCard.module.css';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "../../styles/DestinationCard.module.css";
 
 function DestinationCard({ destination }) {
   const [showComments, setShowComments] = useState(false);
-  const [newComment, setNewComment] = useState('');
+  const [newComment, setNewComment] = useState("");
 
   const handleSubmitComment = (e) => {
     e.preventDefault();
-    // This would typically make an API call to your Django backend
-    console.log('New comment:', newComment);
-    setNewComment('');
+    console.log("New comment:", newComment);
+    setNewComment("");
   };
 
   return (
     <div className={styles.card}>
-      <img src={destination.image} alt={destination.name} className={styles.image} />
+      <img
+        src={destination.image}
+        alt={destination.name}
+        className={styles.image}
+      />
       <div className={styles.content}>
         <h2>{destination.name}</h2>
         <span className={styles.category}>{destination.category}</span>
         <p>{destination.description}</p>
-        <div className={styles.rating}>
-          Rating: {destination.rating} / 5
-        </div>
-        
-        <button 
+        <div className={styles.rating}>Rating: {destination.rating} / 5</div>
+
+        <button
           onClick={() => setShowComments(!showComments)}
           className={styles.commentToggle}
         >
-          {showComments ? 'Hide Comments' : 'Show Comments'}
+          {showComments ? "Hide Comments" : "Show Comments"}
         </button>
 
         {showComments && (
@@ -45,7 +46,7 @@ function DestinationCard({ destination }) {
                 Add Comment
               </button>
             </form>
-            
+
             {destination.comments.map((comment, index) => (
               <div key={index} className={styles.comment}>
                 {comment}
